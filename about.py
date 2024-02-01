1ce67fd905c69553710aedda7a9d43eb9a40937d
Saimaheswari <124231032+Saimaheswari@users.noreply.github.com>
2024-02-01 14:43:12 +0530

    about.py

--- about.py
+++ about.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 
 # Set page title
 st.set_page_config(
-    page_title="Vitality Lung Predictor....",
+    page_title="Sentiment Analysis on Social Media...",
     initial_sidebar_state="expanded",
 )
 st.markdown(""" <style>
@@ -16,5 +16,11 @@ st.write('<style>div.row-widget.stMarkdown { font-size: 24px; }</style>', unsafe
 
 
 
-st.markdown("## Vitality Lung Predictor ")
-st.write("Vitality Lung Predictor, a cutting-edge app harnessing advanced AI for precise lung cancer prediction and classification. Swiftly analyzing medical images, it distinguishes between benign and malignant tumors, further categorizing into specific types such as large cell carcinoma, adenocarcinoma, squamous, and normal lung tissue. Empowering early detection and personalized treatment strategies.")
+st.markdown("## Sentiment Analysis on Social media ")
+st.write("Sentiment analysis on Twitter involves analyzing tweets to determine the emotional tone expressed by users. Natural Language Processing (NLP) algorithms classify tweets as positive, negative, or neutral based on language cues. This process helps businesses gauge public opinion, track brand perception, and identify emerging trends. Challenges include handling sarcasm, slang, and context nuances, making it crucial for models to continuously adapt to evolving language use. Overall, Twitter sentiment analysis plays a pivotal role in understanding the dynamic and real-time sentiments of a diverse online community.
+
+
+
+
+
+")

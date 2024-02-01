c989d669f41dd18c1833a7af665c6e6b05e71c4b
Saimaheswari <124231032+Saimaheswari@users.noreply.github.com>
2024-02-01 14:44:09 +0530

    about.py

--- about.py
+++ about.py
@@ -17,10 +17,4 @@ st.write('<style>div.row-widget.stMarkdown { font-size: 24px; }</style>', unsafe
 
 
 st.markdown("## Sentiment Analysis on Social media ")
-st.write("Sentiment analysis on Twitter involves analyzing tweets to determine the emotional tone expressed by users. Natural Language Processing (NLP) algorithms classify tweets as positive, negative, or neutral based on language cues. This process helps businesses gauge public opinion, track brand perception, and identify emerging trends. Challenges include handling sarcasm, slang, and context nuances, making it crucial for models to continuously adapt to evolving language use. Overall, Twitter sentiment analysis plays a pivotal role in understanding the dynamic and real-time sentiments of a diverse online community.
-
-
-
-
-
-")
+st.write("Sentiment analysis on Twitter involves analyzing tweets to determine the emotional tone expressed by users. Natural Language Processing (NLP) algorithms classify tweets as positive, negative, or neutral based on language cues. This process helps businesses gauge public opinion, track brand perception, and identify emerging trends. Challenges include handling sarcasm, slang, and context nuances, making it crucial for models to continuously adapt to evolving language use. Overall, Twitter sentiment analysis plays a pivotal role in understanding the dynamic and real-time sentiments of a diverse online community.")
